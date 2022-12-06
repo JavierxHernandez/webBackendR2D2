12ff00fb3fbe1a6c9ecd6064fefc3bcca7cc3bb1
Cirel Romeo <cirelhromeo@gmail.com>
2022-12-06 18:03:19 -0300

    Add: DescriptionSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             ImagesTypesSeeder::class,
             ProductsSeeder::class,
             ProvidersSeeder::class,
+            DescriptionsSeeder::class
         ]);
     }
 }
